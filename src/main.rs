620f9d5193510b3f8c17e696aa8d3b474594762b
root <cnnoizb@qq.com>
2024-08-10 17:59:15 +0000

    bump version to 0.10.2

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ use crate::submods::silist;
 #[command(
     name = "rua",
     author = "bzhao",
-    version = "0.10.1",
+    version = "0.10.2",
     about = "Devbox for StoneOS project",
     long_about = None
 )]
