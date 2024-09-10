e8e2f151600edb3bffc4177b969d02c8d305184a
bzhao <cnnoizb@qq.com>
2024-09-10 06:53:17 +0000

    bump version to 0.11.2

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ const STYLES: styling::Styles = styling::Styles::styled()
 #[command(
     name = "rua",
     author = "bzhao",
-    version = "0.11.1",
+    version = "0.11.2",
     styles = STYLES,
     about = "Devbox for StoneOS project",
     long_about = "Devbox for StoneOS project",
