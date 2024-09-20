d7cba466b95947f9d921073e7be65f2c9fb15a4c
bzhao <cnnoizb@qq.com>
2024-09-20 05:54:43 +0000

    use filter_map to replace for loop

--- src/submods/showcc.rs
+++ src/submods/showcc.rs
@@ -1,5 +1,5 @@
 use std::fs;
-use std::path::Path;
+use std::path;
 
 use anyhow::{Context, Result};
 use crossterm::terminal;
@@ -12,24 +12,23 @@ const COLOR_ANSI_YLW: anstyle::Style =
     anstyle::Style::new().fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Yellow)));
 
 /// Fetch the corresponding compile command from compilation database for the given filename.
-pub fn fetch_compile_command(filename: &str, compdb: &Path) -> Result<Vec<CompDBRecord>> {
+pub fn fetch_compile_command(filename: &str, compdb: &path::Path) -> Result<Vec<CompDBRecord>> {
     let compdb_str = fs::read_to_string(compdb)
         .context(format!(r#"Error reading file "{}""#, compdb.display()))?;
     let compdb: CompDB = serde_json::from_str(&compdb_str)
         .context(format!(r#"Error parsing "{}"!"#, compdb.display()))?;
-    let mut commands = vec![];
 
-    for item in compdb.into_iter() {
-        let file = Path::new(item.file.as_str());
-        let basename = file
-            .file_name()
-            .context("Error path format")?
-            .to_str()
-            .context("Invalid Unicode")?;
-        if basename == filename {
-            commands.push(item);
-        }
-    }
+    let commands: Vec<CompDBRecord> = compdb
+        .into_iter()
+        .filter_map(|x| {
+            let file = path::Path::new(x.file.as_str());
+            if file.file_name().unwrap().to_str().unwrap() == filename {
+                Some(x)
+            } else {
+                None
+            }
+        })
+        .collect();
 
     Ok(commands)
 }
