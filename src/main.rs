cf2679696a7a66aa1a81f4338d96fd9677487bbb
Your Name <you@example.com>
2024-10-17 11:14:26 +0800

    bump to 0.13.0

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ const STYLES: styling::Styles = styling::Styles::styled()
 #[command(
     name = "rua",
     author = "bzhao",
-    version = "0.12.0",
+    version = "0.13.0",
     styles = STYLES,
     about = "Devbox for StoneOS project",
     long_about = "Devbox for StoneOS project",
