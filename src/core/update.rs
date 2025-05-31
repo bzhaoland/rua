bc8569a1bf33ab22d849e5e4a95ae21c477ad304
bzhao <cnnoizb@qq.com>
2025-05-31 22:24:04 +0800

    remove tempfile dependency

--- src/core/update.rs
+++ src/core/update.rs
@@ -1,5 +1,4 @@
 use std::fs;
-use std::io::Write;
 use std::os::unix::fs::PermissionsExt;
 
 use anyhow::Context;
@@ -37,45 +36,33 @@ pub(crate) fn update(version: Option<String>) -> anyhow::Result<()> {
                 o.max(Version::parse(&i.version).unwrap())
             })
             .to_string();
-        pbar.finish_with_message("found version ".to_string() + latest_version.as_str());
+        pbar.set_style(ProgressStyle::with_template("Found version {msg}")?);
+        pbar.finish_with_message(latest_version.clone());
         latest_version
     };
 
     let pbar = ProgressBar::no_length().with_style(ProgressStyle::with_template(
-        format!("Downloading rua {}...", target_version).as_str(),
+        format!("Installing rua {}...", target_version).as_str(),
     )?);
     pbar.tick();
     let home = home_dir().context("Failed to get current user's home dir")?;
     let bin_dir = home.join(".local/bin");
     fs::create_dir_all(bin_dir.as_path())
         .context(format!("Failed to create dir {}", bin_dir.display()))?;
+    let dest = bin_dir.join("rua");
     let data = ftp_stream
         .retr_as_buffer(&format!("rua/{}/rua", target_version))?
         .into_inner();
-    let mut temp =
-        tempfile::NamedTempFile::new_in(bin_dir.as_path()).context("Failed to create tempfile")?;
-    temp.write_all(&data)
-        .context("Failed to write local file")?;
-
-    pbar.set_style(ProgressStyle::with_template(
-        format!("Installing rua {}...", target_version).as_str(),
-    )?);
-    pbar.tick();
-    let mut perm = fs::metadata(temp.path())
-        .context(format!("Failed to stat {}", temp.path().display()))?
+    fs::write(dest.as_path(), data.as_slice()).context("Failed to write the binary")?;
+    let mut perm = fs::metadata(dest.as_path())
+        .context(format!("Failed to stat {}", dest.display()))?
         .permissions();
     perm.set_mode(0o755);
-    fs::set_permissions(temp.path(), perm)?;
-    let dest = bin_dir.join("rua");
-    fs::rename(temp.path(), dest.as_path()).context(format!(
-        "Failed to rename {} to {}",
-        temp.path().display(),
-        dest.display()
-    ))?;
+    fs::set_permissions(dest.as_path(), perm)?;
     pbar.set_style(ProgressStyle::with_template(
-        "Updated rua to version {msg}",
+        format!("Updated rua to version {}", target_version).as_str(),
     )?);
-    pbar.finish_with_message(target_version);
+    pbar.finish();
 
     ftp_stream.quit().context("Failed to quit ftp stream")?;
     Ok(())
